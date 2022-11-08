71bb627a50dfecf808fc14e83ea3dcc9b4c661a6
bob-huang <bob83727@gmail.com>
2022-11-08 11:33:12 +0800

    modify ratings

--- ratings.php
+++ ratings.php
@@ -1,7 +1,12 @@
 <?php
 require_once("../housetunedbconnect.php");
-
-if(isset($_GET["startdate"]) && $_GET["startdate"]!==""){
+if(isset($_GET["user_id"])){
+    $id=$_GET["user_id"];
+    $sql= "SELECT rating.*, product.name, user.account FROM rating
+    JOIN product ON rating.product_id = product.id
+    JOIN user ON rating.user_id = user.id
+    WHERE rating.user_id='$id' ";
+}else if(isset($_GET["startdate"]) && $_GET["startdate"]!==""){
     $date1=$_GET["startdate"];
     if(isset($_GET["enddate"]) && $_GET["enddate"]!==""){
     $date2=$_GET["enddate"];
@@ -54,34 +59,53 @@ $rows = $result->fetch_all(MYSQLI_ASSOC);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link href="/fontawesome-free-6.2.0-web/css/all.min.css" rel="stylesheet">
+    <style>
+    .input-group {
+        width: 60%;
+        margin: 40px auto 0;
+    }
+    </style>
 
 </head>
 
 <body>
     <div class="container">
         <div class="py-2">
-        <h2 class="text-center">Housetune商品評價列表</h2>
-        <form action="ratings.php" method="get">
-            <div class="input-group">
-                <input type="text" class="form-control" name="search" value="<?php if(isset($search)) echo"$search";?>" placeholder="依帳號篩選">
-                <h6 class="d-flex align-items-center mx-2">依發布日期篩選</h6>
-                <input type="date" class="form-control" name="startdate"
-                    value="<?php if(isset($date1)) echo"$date1";?>">
-                <h6 class="d-flex align-items-center mx-2">~</h6>
-                <input type="date" class="form-control" name="enddate" value="<?php if(isset($date2)) echo"$date2";?>">
-                <select name="score" id="" class="ms-2">
-                    <option value="">依評分篩選</option>
-                    <option value="5" <?php if(isset($score) && $score=="5") echo"selected"?>>5星</option>
-                    <option value="4" <?php if(isset($score) && $score=="4") echo"selected"?>>4星</option>
-                    <option value="3" <?php if(isset($score) && $score=="3") echo"selected"?>>3星</option>
-                    <option value="2" <?php if(isset($score) && $score=="2") echo"selected"?>>2星</option>
-                    <option value="1" <?php if(isset($score) && $score=="1") echo"selected"?>>1星</option>
+            <?php if(isset($_GET["user_id"])): ?>
+            <h2 class="text-center">會員<?=$rows[0]["account"]?>的所有評價</h2>
+            <?php endif; ?>
+            <?php if(!isset($_GET["user_id"])): ?>
+            <h2 class="text-center">Housetune商品評價列表</h2>
+            <form action="ratings.php" method="get">
+                <div class="input-group">
+                    <h6 class="d-flex align-items-center mx-2">依發布日期篩選</h6>
+                    <input type="date" class="form-control" name="startdate"
+                        value="<?php if(isset($date1)) echo"$date1";?>">
+                    <h6 class="d-flex align-items-center mx-2">~</h6>
+                    <input type="date" class="form-control" name="enddate"
+                        value="<?php if(isset($date2)) echo"$date2";?>">
+                    <select name="score" id="" class="ms-2">
+                        <option value="">依評分篩選</option>
+                        <option value="5" <?php if(isset($score) && $score=="5") echo"selected"?>>5星</option>
+                        <option value="4" <?php if(isset($score) && $score=="4") echo"selected"?>>4星</option>
+                        <option value="3" <?php if(isset($score) && $score=="3") echo"selected"?>>3星</option>
+                        <option value="2" <?php if(isset($score) && $score=="2") echo"selected"?>>2星</option>
+                        <option value="1" <?php if(isset($score) && $score=="1") echo"selected"?>>1星</option>
                     </select>
-                <button class="btn btn-info" type="submit">送出</button>
-            </div>
-        </form>
+                    <button class="btn btn-info" type="submit">送出</button>
+                </div>
+            </form>
+            <?php endif; ?>
         </div>
-        <h6 class="text-start">共 <?= $ratingCount ?> 則</h6>
+        <div class="d-flex justify-content-start my-2">
+            <h6 class="text-start d-flex align-items-center me-4">共 <?= $ratingCount ?> 則</h6>
+            <?php if(isset($_GET["user_id"])): ?>
+            <a href="user-detail.php?id=<?=$id?>" class="btn btn-info">回會員詳細資訊</a>
+            <?php endif; ?>
+            <?php if(isset($_GET["startdate"]) || isset($_GET["enddate"]) || isset($_GET["score"])):?>
+            <a href="ratings.php" class="btn btn-info">回評價列表</a>
+            <?php endif; ?>
+        </div>
         <table class="table table-bordered mt-3">
             <tr>
                 <th>編號</th>
@@ -89,7 +113,7 @@ $rows = $result->fetch_all(MYSQLI_ASSOC);
                 <th>商品名</th>
                 <th>評分</th>
                 <th>發布時間</th>
-                <th></th>
+                <th><?php if(isset($_GET["user_id"])){echo "評論";} ?></th>
             </tr>
             <?php foreach($rows as $row): ?>
             <tr>
@@ -110,7 +134,14 @@ $rows = $result->fetch_all(MYSQLI_ASSOC);
                 ?>
                 </td>
                 <td><?=$row["posted_at"]?></td>
-                <td><a href="rating-detail.php?id=<?=$row["id"]?>" class="btn btn-info">查看詳細評論</a></td>
+                <td>
+                    <?php if(!isset($_GET["user_id"])): ?>
+                    <a href="rating-detail.php?id=<?=$row["id"]?>" class="btn btn-info">查看詳細評論</a>
+                    <?php endif; ?>
+                    <?php if(isset($_GET["user_id"])): ?>
+                    <?=$row["comment"]?>
+                    <?php endif; ?>
+                </td>
             </tr>
             <?php endforeach; ?>
         </table>
