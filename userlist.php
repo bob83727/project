4beaae14dd3f5b58e04392422cd5e56279f9382f
bob-huang <bob83727@gmail.com>
2022-11-08 15:46:09 +0800

    新增userlist分頁功能

--- userlist.php
+++ userlist.php
@@ -2,39 +2,68 @@
 require_once("../housetunedbconnect.php");
 //pdo寫法
 //require_once("../htpdo-connect.php");
-if(isset($_GET["search"]) && $_GET["search"]!==""){
-    $search=$_GET["search"];
-    $sql= "SELECT * FROM user WHERE account LIKE '%$search%' ORDER BY created_at DESC ";}
-else if(isset($_GET["startdate"]) && $_GET["startdate"]!==""){
-    $date1=$_GET["startdate"];
-    if(isset($_GET["enddate"]) && $_GET["enddate"]!==""){
-    $date2=$_GET["enddate"];
-    $sql= "SELECT * FROM user WHERE created_at >= '$date1' AND created_at<= '$date2' ORDER BY created_at DESC ";
-    }else{
-        $sql= "SELECT * FROM user WHERE created_at >= '$date1' ORDER BY created_at DESC ";
-    }
-}else if(isset($_GET["enddate"]) && $_GET["enddate"]!==""){
-    $date2=$_GET["enddate"];
-    $sql= "SELECT * FROM user WHERE created_at<= '$date2' ORDER BY created_at DESC ";
-}
-else if(isset($_GET["address"]) && $_GET["address"]!==""){
-    $address=$_GET["address"];
-    $sql= "SELECT * FROM user WHERE address LIKE '%$address%' ORDER BY created_at DESC ";}
-else if(isset($_GET["status"]) && $_GET["status"]!==""){
-    $valid=$_GET["status"];
-    $sql= "SELECT * FROM user WHERE valid=$valid ORDER BY created_at DESC ";
-}
-else{
-    $sql = "SELECT * FROM user";}
 if(isset($_GET["page"])){
     $page=$_GET["page"];
 }else{
     $page=1;
 }
+$perPage=5;
+$page_start=($page-1)*$perPage;
+
+if(isset($_GET["search"]) && $_GET["search"]!==""){
+    $search=$_GET["search"];
+    $sql = "SELECT * FROM user WHERE account LIKE '%$search%' ORDER BY created_at DESC ";
+    $sql1= "SELECT * FROM user WHERE account LIKE '%$search%' ORDER BY created_at DESC LIMIT $page_start, $perPage";
+}
+else if(isset($_GET["startdate"]) && $_GET["startdate"]!==""){
+    $date1=$_GET["startdate"];
+    if(isset($_GET["enddate"]) && $_GET["enddate"]!==""){
+    $date2=$_GET["enddate"];
+    $sql = "SELECT * FROM user WHERE created_at >= '$date1' AND created_at<= '$date2' ORDER BY created_at DESC ";
+    $sql1= "SELECT * FROM user WHERE created_at >= '$date1' AND created_at<= '$date2' ORDER BY created_at DESC LIMIT $page_start, $perPage";
+    }else{
+        $sql = "SELECT * FROM user WHERE created_at >= '$date1' ORDER BY created_at DESC ";
+        $sql1= "SELECT * FROM user WHERE created_at >= '$date1' ORDER BY created_at DESC $page_start, $perPage";
+    }
+}else if(isset($_GET["enddate"]) && $_GET["enddate"]!==""){
+    $date2=$_GET["enddate"];
+    $sql = "SELECT * FROM user WHERE created_at<= '$date2' ORDER BY created_at DESC ";
+    $sql1= "SELECT * FROM user WHERE created_at<= '$date2' ORDER BY created_at DESC LIMIT $page_start, $perPage";
+}
+else if(isset($_GET["address"]) && $_GET["address"]!==""){
+    $address=$_GET["address"];
+    $sql = "SELECT * FROM user WHERE address LIKE '%$address%' ORDER BY created_at DESC ";
+    $sql1= "SELECT * FROM user WHERE address LIKE '%$address%' ORDER BY created_at DESC LIMIT $page_start, $perPage";}
+else if(isset($_GET["status"]) && $_GET["status"]!==""){
+    $valid=$_GET["status"];
+    $sql = "SELECT * FROM user WHERE valid=$valid ORDER BY created_at DESC ";
+    $sql1= "SELECT * FROM user WHERE valid=$valid ORDER BY created_at DESC LIMIT $page_start, $perPage ";
+}
+else{
+    $sql = "SELECT * FROM user";
+    $sql1= "SELECT * FROM user ORDER BY created_at DESC LIMIT $page_start, $perPage";}
 
 $result = $conn->query($sql);
 $userCount = $result->num_rows;
-$rows = $result->fetch_all(MYSQLI_ASSOC);
+$totalPage=ceil($userCount/$perPage);
+$result1=$conn->query($sql1);
+$rows = $result1->fetch_all(MYSQLI_ASSOC);
+
+// if(isset($_GET["page"])){
+//     $page=$_GET["page"];
+// }else{
+//     $page=1;
+// }
+// //計算所有使用者
+// $sqlAll="SELECT * FROM users WHERE valid=1";
+// $resultAll=$conn->query($sqlAll);
+// $userCount=$resultAll->num_rows;
+// $perPage=5;
+// //$page=3;
+// $page_start=($page-1)*$perPage;
+// $sql="SELECT * FROM users WHERE valid=1 ORDER BY created_at DESC LIMIT $page_start, $perPage";
+// $result=$conn->query($sql);
+// $totalPage=ceil($userCount/$perPage);
 
 //pdo寫法
 // $stmt=$db_host->prepare($sql);
@@ -78,36 +107,39 @@ $rows = $result->fetch_all(MYSQLI_ASSOC);
         <div class="py-2">
             <form action="userlist.php" method="get">
                 <div class="input-group">
-                    <input type="text" class="form-control" name="search" value="<?php if(isset($search)) echo"$search";?>" placeholder="依帳號篩選">
+                    <input type="text" class="form-control" name="search"
+                        value="<?php if(isset($search)) echo"$search";?>" placeholder="依帳號篩選">
                     <h6 class="d-flex align-items-center mx-2">依註冊日期篩選</h6>
-                    <input type="date" class="form-control" name="startdate" value="<?php if(isset($date1)) echo"$date1";?>" >
+                    <input type="date" class="form-control" name="startdate"
+                        value="<?php if(isset($date1)) echo"$date1";?>">
                     <h6 class="d-flex align-items-center mx-2">~</h6>
-                    <input type="date" class="form-control" name="enddate" value="<?php if(isset($date2)) echo"$date2";?>">
+                    <input type="date" class="form-control" name="enddate"
+                        value="<?php if(isset($date2)) echo"$date2";?>">
                     <select name="address" id="" class="ms-2">
-                    <option value="">依城市篩選</option>
-                    <option value="台北市" <?php if(isset($address) && $address=="台北市") echo"selected"?>>台北市</option>
-                    <option value="新北市" <?php if(isset($address) && $address=="新北市") echo"selected"?>>新北市</option>
-                    <option value="台中市" <?php if(isset($address) && $address=="台中市") echo"selected"?>>台中市</option>
-                    <option value="台南市" <?php if(isset($address) && $address=="台南市") echo"selected"?>>台南市</option>
-                    <option value="桃園市" <?php if(isset($address) && $address=="桃園市") echo"selected"?>>桃園市</option>
+                        <option value="">依城市篩選</option>
+                        <option value="台北市" <?php if(isset($address) && $address=="台北市") echo"selected"?>>台北市</option>
+                        <option value="新北市" <?php if(isset($address) && $address=="新北市") echo"selected"?>>新北市</option>
+                        <option value="台中市" <?php if(isset($address) && $address=="台中市") echo"selected"?>>台中市</option>
+                        <option value="台南市" <?php if(isset($address) && $address=="台南市") echo"selected"?>>台南市</option>
+                        <option value="桃園市" <?php if(isset($address) && $address=="桃園市") echo"selected"?>>桃園市</option>
                     </select>
                     <select name="status" id="">
-                    <option value="">依狀態篩選</option>
-                    <option value="1" <?php if(isset($valid) && $valid=='1') echo"selected"?>>使用中</option>
-                    <option value="0" <?php if(isset($valid) && $valid=='0') echo"selected"?>>已停權</option>
+                        <option value="">依狀態篩選</option>
+                        <option value="1" <?php if(isset($valid) && $valid=='1') echo"selected"?>>使用中</option>
+                        <option value="0" <?php if(isset($valid) && $valid=='0') echo"selected"?>>已停權</option>
                     </select>
                     <button class="btn btn-info" type="submit">送出</button>
                 </div>
-                
+
             </form>
         </div>
         <div class="py-2 d-flex justify-content-between">
-        <?php if (isset($search) && $search!==""):?>     
-        <h6><?=$search?>的搜尋結果</h6>
-        <?php endif;?>
-        <?php if (isset($address) && $address!==""):?>     
-        <h6><?=$address?>的搜尋結果</h6>
-        <?php endif;?>
+            <?php if (isset($search) && $search!==""):?>
+            <h6><?=$search?>的搜尋結果</h6>
+            <?php endif;?>
+            <?php if (isset($address) && $address!==""):?>
+            <h6><?=$address?>的搜尋結果</h6>
+            <?php endif;?>
             <h6>共 <?= $userCount ?> 人</h6>
         </div>
         <table class="table table-bordered mt-3">
@@ -138,10 +170,18 @@ $rows = $result->fetch_all(MYSQLI_ASSOC);
                 <?php endforeach; ?>
             </tbody>
         </table>
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <?php for($i=1; $i<=$totalPage; $i++):?>
+                <li class="page-item <?php if($i==$page)echo "active";?>"><a class="page-link"
+                        href="userlist.php?page=<?=$i?>&search=<?php if(isset($search)) echo"$search";?>&startdate=<?php if(isset($date1)) echo"$date1";?>&enddate=<?php if(isset($date2)) echo"$date2";?>&address=<?php if(isset($address)) echo"$address";?>&status=<?php if(isset($valid)) echo"$valid";?>"><?=$i?></a></li>
+                <?php endfor; ?>
+            </ul>
+        </nav>
         <?php if(isset($search)|| isset($address)|| isset($valid)|| isset($date1)|| isset($date2)):?>
-            <div>
-                <a href="userlist.php" class="btn btn-info">回全部會員列表</a>
-            </div>
+        <div>
+            <a href="userlist.php" class="btn btn-info">回全部會員列表</a>
+        </div>
         <?php endif ;?>
     </div>
 </body>
